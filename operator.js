76f2900cb09651cb5db85d8e076e0e786de674d0
xellosPark <84718267+xellosPark@users.noreply.github.com>
2021-08-03 16:58:06 +0900

    operator.js [add]

---
+++ operator.js
@@ -0,0 +1,14 @@
+// 1. String concatenation
+console.log('my'+'cat');
+console.log('1' + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+console.log('pakr\'s book');
+console.log("pakr's \n\tbook"); //\n 개행 \t tap key
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 2); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
