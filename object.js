096e91a260609695e0fc15e12e36518c4190ffa4
xellosPark <84718267+xellosPark@users.noreply.github.com>
2021-08-10 22:46:04 +0900

    Object [add]

--- object.js
+++ object.js
@@ -38,3 +38,79 @@ function printValue(obj, key) {
 }
 
 printValue(park, 'name');
+printValue(park, 'age');
+
+//3. Proety value shorthand
+const person1 ={ name: 'bob', age: 2};
+const person2 ={ name: 'steve', age: 3};
+const person3 ={ name: 'dave', age: 4};
+const person4 = makePerson('park', 30);
+console.log(person4);
+
+function makePerson(name, age){
+    return {
+        // name: name,
+        // age: age,
+        // key, value 값이 같은 경우에 인자값을 생략가능하다.
+        name,
+        age,
+
+    };
+}
+// 4. Constutor 함수 
+const person5 = new person('dong', 30);
+console.log(person5);
+// object 자동으로 생성
+function person(name, age){
+    // this = {};
+    this.name = name;
+    this.age = age;
+    // 결국 return this;
+}
+
+// 5. in operator: property existence check (key in obj)
+// odj key 여부 확인
+console.log('name' in park);
+console.log('age' in park);
+console.log('random' in park);
+console.log(park.random);
+
+//6. for..in vs for..of
+// for (key in obj)
+console.clear();
+for (key in park){
+    console.log(key);
+}
+
+// for (value of iterable)
+const array = [1,2,4,5];
+for (value of array){
+    console.log(value);
+}
+
+//7. Fun cloning
+// Objec.assign(dest, [obj1, obj2, obj3...])
+const user = { name: 'park', age: '20'};
+const user2 = user;
+user2.name = 'coder';
+console.log(user);
+
+//old way
+const user3 = {};
+for (key in user) {
+    user3[key] = user[key];
+}
+console.log(user3);
+
+const user4 = {}; 
+Object.assign(user4, user);
+console.log(user4);
+
+//const user4 = Object.assign({}, user); 
+
+// another example
+const fruit1 = { color: 'red'};
+const fruit2 = { color: 'blue', size: 'big'};
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size); 
