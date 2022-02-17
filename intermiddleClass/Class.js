0474d4a8d8fdea20387a236a00732a1029b86194
ParkDongJun <yuki@ubisam.com>
2022-02-17 13:53:06 +0900

    Class ADD

--- intermiddleClass/Class.js
+++ intermiddleClass/Class.js
@@ -5,7 +5,6 @@ const Uesr = function (name, age){
         console.log(this.name);
     };
 };
-
 const mike = new Uesr('Mike', 30);
 
 class User2{
@@ -17,5 +16,37 @@ class User2{
         console.log(this.name);
     }
 }
-
 const tom = new User2("Tom",19);
+
+// extends
+class Car {
+    constructor(color){
+        this.color = color;
+        this.wheels = 4;
+    }
+    drive() {
+        console.log("drive..");
+    }
+    stop() {
+        console.log("STOP!");
+    }
+}
+// 상속
+class Bmw extends Car{
+    //constructor(...args){
+        //super(...args);
+    constructor(color){
+        super(color);
+        this.navigation = 1;
+    }
+    park() {
+        console.log("PARK");
+    }
+    stop(){
+        super();
+        // super.stop();
+        console.log("OFF");
+    }
+}
+
+const z4 = new Bmw("blue");
